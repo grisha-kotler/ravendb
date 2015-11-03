584008142157c62be48bdc5906cdb355edb2e7f0
Grisha Kotler <grisha@ayende.com>
2015-11-03 15:58:25 +0200

    added test

--- Raven.Tests.Counters/CountersChangesTests.cs
+++ Raven.Tests.Counters/CountersChangesTests.cs
@@ -24,7 +24,7 @@ namespace Raven.Tests.Counters
                 var changes = store.Changes();
                 var notificationTask = changes.Task.Result
                     .ForChange(GroupName, CounterName)
-                    .Timeout(TimeSpan.FromSeconds(300))
+                    .Timeout(TimeSpan.FromSeconds(5))
                     .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
@@ -38,7 +38,7 @@ namespace Raven.Tests.Counters
 
                 notificationTask = changes.Task.Result
                     .ForChange(GroupName, CounterName)
-                    .Timeout(TimeSpan.FromSeconds(300))
+                    .Timeout(TimeSpan.FromSeconds(5))
                     .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
@@ -58,33 +58,33 @@ namespace Raven.Tests.Counters
             using (var store = NewRemoteCountersStore(DefaultCounterStorageName))
             {
                 var changes = store.Changes();
-                var notificationTask1 = changes.Task.Result
+                var notificationTask = changes.Task.Result
                     .ForCountersStartingWith(GroupName, CounterName.Substring(0, 2))
-                    .Timeout(TimeSpan.FromSeconds(300))
+                    .Timeout(TimeSpan.FromSeconds(5))
                     .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
                 await store.ChangeAsync(GroupName, CounterName, 2);
 
-                var counterChange1 = await notificationTask1;
-                Assert.Equal(GroupName, counterChange1.GroupName);
-                Assert.Equal(CounterName, counterChange1.CounterName);
-                Assert.Equal(CounterChangeAction.Add, counterChange1.Action);
-                Assert.Equal(2, counterChange1.Total);
+                var counterChange = await notificationTask;
+                Assert.Equal(GroupName, counterChange.GroupName);
+                Assert.Equal(CounterName, counterChange.CounterName);
+                Assert.Equal(CounterChangeAction.Add, counterChange.Action);
+                Assert.Equal(2, counterChange.Total);
                 
-                var notificationTask2 = changes.Task.Result
+                notificationTask = changes.Task.Result
                  .ForCountersStartingWith(GroupName, CounterName)
-                 .Timeout(TimeSpan.FromSeconds(300))
+                 .Timeout(TimeSpan.FromSeconds(5))
                  .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
                 await store.DecrementAsync(GroupName, CounterName2);
 
-                var counterChange2 = await notificationTask2;
-                Assert.Equal(GroupName, counterChange2.GroupName);
-                Assert.Equal(CounterName2, counterChange2.CounterName);
-                Assert.Equal(CounterChangeAction.Add, counterChange2.Action);
-                Assert.Equal(-1, counterChange2.Total);
+                counterChange = await notificationTask;
+                Assert.Equal(GroupName, counterChange.GroupName);
+                Assert.Equal(CounterName2, counterChange.CounterName);
+                Assert.Equal(CounterChangeAction.Add, counterChange.Action);
+                Assert.Equal(-1, counterChange.Total);
             }
         }
 
@@ -114,7 +114,7 @@ namespace Raven.Tests.Counters
 
                 var notificationTask2 = changes.Task.Result
                  .ForCountersStartingWith(GroupName, CounterName)
-                 .Timeout(TimeSpan.FromSeconds(300))
+                 .Timeout(TimeSpan.FromSeconds(5))
                  .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
@@ -136,7 +136,7 @@ namespace Raven.Tests.Counters
                 var changes = store.Changes();
                 var notificationTask = changes.Task.Result
                     .ForCountersInGroup(GroupName)
-                    .Timeout(TimeSpan.FromSeconds(300))
+                    .Timeout(TimeSpan.FromSeconds(5))
                     .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
@@ -150,7 +150,7 @@ namespace Raven.Tests.Counters
 
                 notificationTask = changes.Task.Result
                     .ForCountersInGroup(GroupName)
-                    .Timeout(TimeSpan.FromSeconds(300))
+                    .Timeout(TimeSpan.FromSeconds(5))
                     .Take(1).ToTask();
 
                 changes.WaitForAllPendingSubscriptions();
@@ -219,7 +219,7 @@ namespace Raven.Tests.Counters
                 var changesB = storeB.Changes();
                 var notificationTask = changesB.Task.Result
                     .ForCountersInGroup(GroupName)
-                    .Timeout(TimeSpan.FromSeconds(30))
+                    .Timeout(TimeSpan.FromSeconds(10))
                     .Take(1).ToTask();
                 await storeA.IncrementAsync(GroupName, CounterName);
 
@@ -234,7 +234,7 @@ namespace Raven.Tests.Counters
                 var changesA = storeA.Changes();
                 notificationTask = changesA.Task.Result
                     .ForCountersInGroup(GroupName)
-                    .Timeout(TimeSpan.FromSeconds(30))
+                    .Timeout(TimeSpan.FromSeconds(10))
                     .Take(1).ToTask();
 
                 changesA.WaitForAllPendingSubscriptions();
